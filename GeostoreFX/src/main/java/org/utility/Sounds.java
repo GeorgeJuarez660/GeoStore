a094cfa71d7b76db240f3174d67d8222a0d94378
GeorgeJuarez <giorgionocerino01@gmail.com>
2025-05-08 22:04:36 +0200

    dynamic directories added (part 2)
    dynamic directories added (part 2 - in playing sounds file)

--- GeostoreFX/src/main/java/org/utility/Sounds.java
+++ GeostoreFX/src/main/java/org/utility/Sounds.java
@@ -3,14 +3,18 @@ package org.utility;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.InputStream;
 
 public class Sounds {
 
     public static void soundGo(){ //suono generato quando viene cliccato il pulsante avanti
         try {
-            File fileAudio = new File("C:/Users/giorg/OneDrive/Desktop/App/G&P/Programming/Java/GeostoreFX/src/main/resources/org/sounds/go.wav"); // Inserisci il tuo file WAV
-            AudioInputStream audioStream = AudioSystem.getAudioInputStream(fileAudio);
+            //File fileAudio = new File("C:/Users/giorg/OneDrive/Desktop/App/G&P/Programming/Java/GeostoreFX/src/main/resources/org/sounds/go.wav"); // Inserisci il tuo file WAV
+            InputStream audioDyPath = Sounds.class.getResourceAsStream("/org/sounds/go.wav"); //inserisco dinamicamente il file audio
+            BufferedInputStream readAudio = new BufferedInputStream(audioDyPath); //leggo dinamicamente il file audio
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(readAudio); //e passo come input audio
             Clip clip = AudioSystem.getClip();
             clip.open(audioStream);
             clip.start(); // Riproduce il suono
@@ -22,8 +26,10 @@ public class Sounds {
 
     public static void soundBack(){ //suono generato quando viene cliccato il pulsante indietro
         try {
-            File fileAudio = new File("C:/Users/giorg/OneDrive/Desktop/App/G&P/Programming/Java/GeostoreFX/src/main/resources/org/sounds/back.wav"); // Inserisci il tuo file WAV
-            AudioInputStream audioStream = AudioSystem.getAudioInputStream(fileAudio);
+            //File fileAudio = new File("C:/Users/giorg/OneDrive/Desktop/App/G&P/Programming/Java/GeostoreFX/src/main/resources/org/sounds/back.wav"); // Inserisci il tuo file WAV
+            InputStream audioDyPath = Sounds.class.getResourceAsStream("/org/sounds/back.wav"); //inserisco dinamicamente il file audio
+            BufferedInputStream readAudio = new BufferedInputStream(audioDyPath); //leggo dinamicamente il file audio
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(readAudio); //e passo come input audio
             Clip clip = AudioSystem.getClip();
             clip.open(audioStream);
             clip.start(); // Riproduce il suono
@@ -35,8 +41,10 @@ public class Sounds {
 
     public static void soundInfo(){ //suono generato quando compare la scena info
         try {
-            File fileAudio = new File("C:/Users/giorg/OneDrive/Desktop/App/G&P/Programming/Java/GeostoreFX/src/main/resources/org/sounds/info.wav"); // Inserisci il tuo file WAV
-            AudioInputStream audioStream = AudioSystem.getAudioInputStream(fileAudio);
+            //File fileAudio = new File("C:/Users/giorg/OneDrive/Desktop/App/G&P/Programming/Java/GeostoreFX/src/main/resources/org/sounds/info.wav"); // Inserisci il tuo file WAV
+            InputStream audioDyPath = Sounds.class.getResourceAsStream("/org/sounds/info.wav"); //inserisco dinamicamente il file audio
+            BufferedInputStream readAudio = new BufferedInputStream(audioDyPath); //leggo dinamicamente il file audio
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(readAudio); //e passo come input audio
             Clip clip = AudioSystem.getClip();
             clip.open(audioStream);
             clip.start(); // Riproduce il suono
@@ -48,8 +56,10 @@ public class Sounds {
 
     public static void soundOpen(){ //suono generato quando si apre il programma
         try {
-            File fileAudio = new File("C:/Users/giorg/OneDrive/Desktop/App/G&P/Programming/Java/GeostoreFX/src/main/resources/org/sounds/open.wav"); // Inserisci il tuo file WAV
-            AudioInputStream audioStream = AudioSystem.getAudioInputStream(fileAudio);
+            //File fileAudio = new File("C:/Users/giorg/OneDrive/Desktop/App/G&P/Programming/Java/GeostoreFX/src/main/resources/org/sounds/open.wav"); // Inserisci il tuo file WAV
+            InputStream audioDyPath = Sounds.class.getResourceAsStream("/org/sounds/open.wav"); //inserisco dinamicamente il file audio
+            BufferedInputStream readAudio = new BufferedInputStream(audioDyPath); //leggo dinamicamente il file audio
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(readAudio); //e passo come input audio
             Clip clip = AudioSystem.getClip();
             clip.open(audioStream);
             clip.start(); // Riproduce il suono
@@ -61,8 +71,10 @@ public class Sounds {
 
     public static void soundClose(){ //suono generato quando si chiude il programma
         try {
-            File fileAudio = new File("C:/Users/giorg/OneDrive/Desktop/App/G&P/Programming/Java/GeostoreFX/src/main/resources/org/sounds/close1.wav"); // Inserisci il tuo file WAV
-            AudioInputStream audioStream = AudioSystem.getAudioInputStream(fileAudio);
+            //File fileAudio = new File("C:/Users/giorg/OneDrive/Desktop/App/G&P/Programming/Java/GeostoreFX/src/main/resources/org/sounds/close1.wav"); // Inserisci il tuo file WAV
+            InputStream audioDyPath = Sounds.class.getResourceAsStream("/org/sounds/close1.wav"); //inserisco dinamicamente il file audio
+            BufferedInputStream readAudio = new BufferedInputStream(audioDyPath); //leggo dinamicamente il file audio
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(readAudio); //e passo come input audio
             Clip clip = AudioSystem.getClip();
             clip.open(audioStream);
             clip.start(); // Riproduce il suono
@@ -72,8 +84,10 @@ public class Sounds {
         }
 
         try {
-            File fileAudio = new File("C:/Users/giorg/OneDrive/Desktop/App/G&P/Programming/Java/GeostoreFX/src/main/resources/org/sounds/close2.wav"); // Inserisci il tuo file WAV
-            AudioInputStream audioStream = AudioSystem.getAudioInputStream(fileAudio);
+            //File fileAudio = new File("C:/Users/giorg/OneDrive/Desktop/App/G&P/Programming/Java/GeostoreFX/src/main/resources/org/sounds/close2.wav"); // Inserisci il tuo file WAV
+            InputStream audioDyPath = Sounds.class.getResourceAsStream("/org/sounds/close2.wav"); //inserisco dinamicamente il file audio
+            BufferedInputStream readAudio = new BufferedInputStream(audioDyPath); //leggo dinamicamente il file audio
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(readAudio); //e passo come input audio
             Clip clip = AudioSystem.getClip();
             clip.open(audioStream);
             clip.start(); // Riproduce il suono
@@ -85,8 +99,10 @@ public class Sounds {
 
     public static void soundWelcome(){ //suono generato quando si accede per la prima volta il menu
         try {
-            File fileAudio = new File("C:/Users/giorg/OneDrive/Desktop/App/G&P/Programming/Java/GeostoreFX/src/main/resources/org/sounds/welcome.wav"); // Inserisci il tuo file WAV
-            AudioInputStream audioStream = AudioSystem.getAudioInputStream(fileAudio);
+            //File fileAudio = new File("C:/Users/giorg/OneDrive/Desktop/App/G&P/Programming/Java/GeostoreFX/src/main/resources/org/sounds/welcome.wav"); // Inserisci il tuo file WAV
+            InputStream audioDyPath = Sounds.class.getResourceAsStream("/org/sounds/welcome.wav"); //inserisco dinamicamente il file audio
+            BufferedInputStream readAudio = new BufferedInputStream(audioDyPath); //leggo dinamicamente il file audio
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(readAudio); //e passo come input audio
             Clip clip = AudioSystem.getClip();
             clip.open(audioStream);
             clip.start(); // Riproduce il suono
@@ -98,8 +114,10 @@ public class Sounds {
 
     public static void soundLogout(){ //suono generato quando si accede per la prima volta il menu
         try {
-            File fileAudio = new File("C:/Users/giorg/OneDrive/Desktop/App/G&P/Programming/Java/GeostoreFX/src/main/resources/org/sounds/logout.wav"); // Inserisci il tuo file WAV
-            AudioInputStream audioStream = AudioSystem.getAudioInputStream(fileAudio);
+            //File fileAudio = new File("C:/Users/giorg/OneDrive/Desktop/App/G&P/Programming/Java/GeostoreFX/src/main/resources/org/sounds/logout.wav"); // Inserisci il tuo file WAV
+            InputStream audioDyPath = Sounds.class.getResourceAsStream("/org/sounds/logout.wav"); //inserisco dinamicamente il file audio
+            BufferedInputStream readAudio = new BufferedInputStream(audioDyPath); //leggo dinamicamente il file audio
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(readAudio); //e passo come input audio
             Clip clip = AudioSystem.getClip();
             clip.open(audioStream);
             clip.start(); // Riproduce il suono
@@ -111,8 +129,10 @@ public class Sounds {
 
     public static void soundLoading(){ //suono generato quando compare la scena del caricamento
         try {
-            File fileAudio = new File("C:/Users/giorg/OneDrive/Desktop/App/G&P/Programming/Java/GeostoreFX/src/main/resources/org/sounds/loading.wav"); // Inserisci il tuo file WAV
-            AudioInputStream audioStream = AudioSystem.getAudioInputStream(fileAudio);
+            //File fileAudio = new File("C:/Users/giorg/OneDrive/Desktop/App/G&P/Programming/Java/GeostoreFX/src/main/resources/org/sounds/loading.wav"); // Inserisci il tuo file WAV
+            InputStream audioDyPath = Sounds.class.getResourceAsStream("/org/sounds/loading.wav"); //inserisco dinamicamente il file audio
+            BufferedInputStream readAudio = new BufferedInputStream(audioDyPath); //leggo dinamicamente il file audio
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(readAudio); //e passo come input audio
             Clip clip = AudioSystem.getClip();
             clip.open(audioStream);
             clip.start(); // Riproduce il suono
@@ -124,8 +144,10 @@ public class Sounds {
 
     public static void soundPositiveAndQuestionNotify(){ //suono generato quando compare la scena della risposta positiva e della domanda
         try {
-            File fileAudio = new File("C:/Users/giorg/OneDrive/Desktop/App/G&P/Programming/Java/GeostoreFX/src/main/resources/org/sounds/positiveAndQuestionNotify.wav"); // Inserisci il tuo file WAV
-            AudioInputStream audioStream = AudioSystem.getAudioInputStream(fileAudio);
+            //File fileAudio = new File("C:/Users/giorg/OneDrive/Desktop/App/G&P/Programming/Java/GeostoreFX/src/main/resources/org/sounds/positiveAndQuestionNotify.wav"); // Inserisci il tuo file WAV
+            InputStream audioDyPath = Sounds.class.getResourceAsStream("/org/sounds/positiveAndQuestionNotify.wav"); //inserisco dinamicamente il file audio
+            BufferedInputStream readAudio = new BufferedInputStream(audioDyPath); //leggo dinamicamente il file audio
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(readAudio); //e passo come input audio
             Clip clip = AudioSystem.getClip();
             clip.open(audioStream);
             clip.start(); // Riproduce il suono
@@ -137,8 +159,10 @@ public class Sounds {
 
     public static void soundNegativeNotify(){ //suono generato quando compare la scena della risposta negativa
         try {
-            File fileAudio = new File("C:/Users/giorg/OneDrive/Desktop/App/G&P/Programming/Java/GeostoreFX/src/main/resources/org/sounds/negativeNotify.wav"); // Inserisci il tuo file WAV
-            AudioInputStream audioStream = AudioSystem.getAudioInputStream(fileAudio);
+            //File fileAudio = new File("C:/Users/giorg/OneDrive/Desktop/App/G&P/Programming/Java/GeostoreFX/src/main/resources/org/sounds/negativeNotify.wav"); // Inserisci il tuo file WAV
+            InputStream audioDyPath = Sounds.class.getResourceAsStream("/org/sounds/negativeNotify.wav"); //inserisco dinamicamente il file audio
+            BufferedInputStream readAudio = new BufferedInputStream(audioDyPath); //leggo dinamicamente il file audio
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(readAudio); //e passo come input audio
             Clip clip = AudioSystem.getClip();
             clip.open(audioStream);
             clip.start(); // Riproduce il suono
